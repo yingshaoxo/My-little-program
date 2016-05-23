d20195b4c09d4ee148eb2b1b17c784a56770ceaf
yingshaoxo <1576570260@qq.com>
2016-05-23 20:51:22 +0800

    update

---
+++ Programming/TRY/GetWebDescription.py
@@ -0,0 +1,34 @@
+#Now, I made it, get a website description.
+
+import requests
+import re
+
+
+def get_web_description(website):
+    r = requests.get(website)
+    title = re_get_first_text("<title>[\s\S]*?</title>", r.text)
+    title = title.replace('<title>', '').replace('</title>', '')
+
+    description = re_get_first_text('''<meta[^>]*name=[\"|\']description[\"|\'][^>]*content=[\"]([^\"]*)[\"][^>]*>''', r.text)
+    description = description.replace(''''<meta name="description" content="''', '').replace(''''<meta name="description"  content="''', '').replace('/', '').replace('"', '').replace('>', '')
+
+    if (title == '' or description == ''):
+        result = title + description
+    else:
+        result = title + '\n\n' + description
+    
+    return result
+
+
+def re_get_first_text(regular_expression, from_text):
+    result_list = re.findall(regular_expression, from_text)
+    if (len(result_list) != 0):
+        return result_list[0]
+    else:
+        return ''
+    
+
+while True:
+    website = input('Input a website and press Enter, please: ')
+    print('\n\n'+get_web_description(website)+'\n\n')
+
