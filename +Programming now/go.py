8ce788dd7a5b87ebb866f5a31068c70e8d759e70
yingshaoxo <1576570260@qq.com>
2016-06-30 22:02:47 +0800

    update

--- +Programming now/go.py
+++ +Programming now/go.py
@@ -1,51 +1,18 @@
-class FenwickTree(object):
-    def __init__(self, n):
-        self.sum_array = [0] * (n + 1)
-        self.n = n
- 
-    def lowbit(self, x):
-        return x & -x
- 
-    def add(self, x, val):
-        while x <= self.n:
-            self.sum_array[x] += val
-            x += self.lowbit(x)
- 
-    def sum(self, x):
-        res = 0
-        while x > 0:
-            res += self.sum_array[x]
-            x -= self.lowbit(x)
-        return res
- 
- 
-class Solution(object):
-    def countRangeSum(self, nums, lower, upper):
-        """
-        :type nums: List[int]
-        :type lower: int
-        :type upper: int
-        :rtype: int
-        """
-        if not nums: return 0
-        sum_array = [upper, lower - 1]
-        total = 0
-        for num in nums:
-            total += num
-            sum_array += [total, total + lower - 1, total + upper]
- 
-        index = {}
-        for i, x in enumerate(sorted(set(sum_array))):
-            index[x] = i + 1
- 
-        tree = FenwickTree(len(index))
-        ans = 0
-        for i in range(len(nums) - 1, -1, -1):
-            tree.add(index[total], 1)
-            total -= nums[i]
-            ans += tree.sum(index[upper + total]) - tree.sum(index[lower + total - 1])
-        return ans
+def get_number(base_number):
+    A = 10*base_number+1
 
-example = Solution()
-result = example.countRangeSum([-2, 5, -1], -2, 2)
-print(result)
+    additional_number = int(base_number/5)
+    if (additional_number != 0):
+        for i in range(0, additional_number-1):
+            A+=i*5
+        A+=base_number%5
+
+    B = A+10
+    B+=additional_number
+    
+    return list(range(A, B)), B-A, additional_number
+
+
+for i in range(4, 21):
+    print(get_number(i))
+
