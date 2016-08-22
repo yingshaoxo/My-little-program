c367c31b5562dcceec4d67a969d9cea2ed298ea0
yingshaoxo <1576570260@qq.com>
2016-08-23 01:58:46 +0800

    update

--- @Xiaoya/Python/@Xiaoya.py
+++ @Xiaoya/Python/@Xiaoya.py
@@ -6,46 +6,94 @@ global path
 path = os.path.dirname(__file__) + '\\'
 
 
-class knowledge:
+class knowledge():
     '''Get knowledge from txt file.'''
     def __init__(self):
-        sources_dir = path + 'Sources\\'
-        
-        if not os.path.exists(sources_dir):
-            os.mkdir(sources_dir)
-            with open(sources_dir + 'setting.json', 'w') as f:
-                f.write(json.dumps({'books': []}, sort_keys=True, indent=4))
+        self.dir = path + 'Sources\\'
 
-        with open(sources_dir + 'setting.json', 'r') as f:
+        def get_setting():
+            with open(self.dir + 'setting.json', 'r') as f:
                 text = f.read()
-        setting = json.loads(text)
-        old_books = setting.get('books')
-
-        txt_files = [i for i in os.listdir(sources_dir) if '.txt' in i]
-        print(txt_files)
-        if old_books == None:
-            new_txt = txt_files
-            old_books = {'NO': 0}
-        else:
-            new_txt = [i for i in txt_files if i not in old_books]
-        for i in new_txt:
-            old_books.update({i: 0})
-        if 'NO' in old_books:
-            del old_books['NO']
+            return json.loads(text)
         
-        setting.update({'books': old_books})
-        with open(sources_dir + 'setting.json', 'w') as f:
-            f.write(json.dumps(setting, sort_keys=True, indent=4))        
-    
-    def get_setting(self):
-        sources_dir = path + 'Sources\\'
-        with open(sources_dir + 'setting.json', 'r') as f:
+        def write_setting(a_dict):
+            with open(self.dir + 'setting.json', 'w') as f:
+                f.write(json.dumps(a_dict, sort_keys=True, indent=4))
+
+        def update_setting():
+            setting = get_setting()
+            old_books = setting.get('books')
+
+            txt_files = [i for i in os.listdir(self.dir) if '.txt' in i]
+            print(txt_files)
+            if old_books == None:
+                new_txt = txt_files
+                old_books = {'NO': 0}
+            else:
+                new_txt = [i for i in txt_files if i not in old_books]
+            
+            for i in new_txt:
+                old_books.update({i: 0})
+            if 'NO' in old_books:
+                del old_books['NO']
+        
+            setting.update({'books': old_books})
+            write_setting(setting)
+
+        if not os.path.exists(self.dir):
+            os.mkdir(self.dir)
+        if not os.path.exists(self.dir + 'setting.json'):
+            write_setting({'books': {'NO': 0}})
+        
+        update_setting()
+
+    def get_knowledge(self):
+        self.dir = path + 'Sources\\'
+
+        def get_setting():
+            with open(self.dir + 'setting.json', 'r') as f:
                 text = f.read()
-        setting = json.loads(text)
-        return setting
+            return json.loads(text)
+        
+        def write_setting(a_dict):
+            with open(self.dir + 'setting.json', 'w') as f:
+                f.write(json.dumps(a_dict, sort_keys=True, indent=4))
 
+        def split_txt(dirname):
+            try:
+                with open(dirname, 'r',  encoding='utf-8', errors='replace') as f:
+                    text = f.read()
+            except Exception as e:
+                print(e)
+                
+            try:
+                return text.split('\n\n' + '——————————————' + '\n\n')
+            except Exception as e:
+                print(e)
+                return text.split('\n')
 
-class xiaoya:
+        def which_part():
+            setting = get_setting()
+            books = setting.get('books')
+
+            import random
+            book = random.choice(list(books.keys()))
+            print(book)
+            num = books.get(book)
+            print(num)
+            part = split_txt(self.dir + book)[num]
+            print(part)
+
+            num += 1
+            books.update({book: num})
+            setting.update({'books': books})
+            print(setting)
+            write_setting(setting)
+
+        return which_part()
+
+ 
+class xiaoya():
     '''A real xiaoya class'''
     def __init__(self, name, age):
         self.name = name
@@ -60,7 +108,7 @@ class xiaoya:
 
     def knowledge(self):
         k = knowledge()
-        print(k.get_files_in_dir())
+        print(k.get_knowledge())
 
 
 x = xiaoya('xiaoya', 17)
