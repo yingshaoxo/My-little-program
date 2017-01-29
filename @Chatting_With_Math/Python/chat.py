6f36f1ad25036b1ec376e97f98a1d28468edd95d
yingshaoxo <1576570260@qq.com>
2017-01-29 20:21:26 +0800

    Now chatserver could show ip

--- @Chatting_With_Math/Python/chat.py
+++ @Chatting_With_Math/Python/chat.py
@@ -9,7 +9,13 @@ from tornado.concurrent import Future
 from tornado import gen
 from tornado.options import define, options, parse_command_line
 
-define("port", default=8888, help="run on the given port", type=int)
+
+import socket
+local_host = socket.gethostbyname(socket.gethostname())
+tip = '''http://{host}:5277'''.format(host=local_host)
+print(tip)
+
+define("port", default=5277, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode")
 
 
