59f72ecd9f53fb1c363f8af4bf0d2e9c800cc80c
yingshaoxo <yingshaoxo@gmail.com>
2017-05-11 10:24:44 +0800

    deleted all useless file

--- +Python Codes/AutoToGit.py
+++
@@ -1,4 +0,0 @@
-import os
-
-commands = ['cd ', 'cd Downloads/My-little-program/', 'git add .', 'git commit -m "update"', 'git push origin master']
-os.system(' && '.join(commands))
--- +Python Codes/AutoToSSH.py
+++
@@ -1,7 +0,0 @@
-import os
-
-ip_address = '45.76.29.42'
-user_name = 'root'
-port = '77'
-commands = ['ssh ' + user_name + '@' + ip_address + '  -p' + port]
-os.system(' && '.join(commands))
